8a293499820b1be68d2c664a4ce71051fd6ed11f
yensgit <mariamyena2@gmail.com>
2024-09-25 10:12:57 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -1,1 +1,8 @@
 # Create your models here.
+ #ville/models.py
+from django.db import models
+class Ville(models.Model):
+  title = models.CharField(max_length=100)
+  nom=models.CharField(max_length=100)
+  code_postal=models.IntegerField(default=0)
+  prix_m2=models.IntegerField(default=0)

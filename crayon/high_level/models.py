6b099d14079fb55b8dc85f6ce9b3822445d04fd2
yensgit <mariamyena2@gmail.com>
2024-09-25 10:50:44 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -6,7 +6,7 @@ class Ville(models.Model):
   nom=models.CharField(max_length=100)
   code_postal=models.IntegerField(default=0)
   prix_m2=models.IntegerField(default=0)
- #local/models.py
+ 
 class Local(models.Model):
   ville= models.ForeignKey(
    Ville,
@@ -14,7 +14,7 @@ class Local(models.Model):
   )
   nom=models.CharField(max_length=100)
   surface=models.IntegerField(default=0)
- #siegesocial/models.py
+ 
 class Siegesocial(models.Model):
- #siegesocial/models.py
+ 
 class Ressource(models.Model):

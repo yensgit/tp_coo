a390510d67b29f5a70e1d5a23e5e2cc430c543d1
yensgit <mariamyena2@gmail.com>
2024-09-25 10:58:30 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -2,20 +2,16 @@
  #ville/models.py
 from django.db import models
 class Ville(models.Model):
-  title = models.CharField(max_length=100)
-  nom=models.CharField(max_length=100)
-  code_postal=models.IntegerField(default=0)
-  prix_m2=models.IntegerField(default=0)
- 
+    title = models.CharField(max_length=100)
+    nom=models.CharField(max_length=100)
+    code_postal=models.IntegerField(default=0)
+    prix_m2=models.IntegerField(default=0)
 class Local(models.Model):
-  ville= models.ForeignKey(
-   Ville,
-   on_delete=models.PROTECT,
-  )
-  nom=models.CharField(max_length=100)
-  surface=models.IntegerField(default=0)
- 
+    ville= models.ForeignKey(
+    Ville,
+    on_delete=models.PROTECT,
+    )
+    nom=models.CharField(max_length=100)
+    surface=models.IntegerField(default=0)
 class Siegesocial(models.Model):
- 
 class Ressource(models.Model):
- 

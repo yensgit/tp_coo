e67548556d9bd3e7a04858aefa9aad29f7e82f97
yensgit <mariamyena2@gmail.com>
2024-10-01 11:35:28 +0200

    Update tests.py

--- crayon/high_level/tests.py
+++ crayon/high_level/tests.py
@@ -15,34 +15,27 @@ class MachineModelTests(TestCase):
     def test_usine_costs(self): 
          v=Ville.objects.create(nom="Labège",
                                code_postal=31000,
-                               prix_m2=2000
-        )
+                               prix_m2=2000)
         u=Usine.objects.create(nom="CHEZ MARIAM",
-                              ville=v,
-                              surface=50
-                              )
+                               ville=v,
+                               surface=50)
         m1=Machine.objects.create(nom="m1",
-                               prix=1000,
-                               n_serie=1)
+                                  prix=1000,
+                                  n_serie=1)
         u.machines.add(m1)
         m2=Machine.objects.create(nom="m2",
-                               prix=2000,
-                               n_serie=2)
+                                  prix=2000,
+                                  n_serie=2)
         u.machines.add(m2) 
         r1=Ressource.objects.create(nom="bois",
-                               prix=10,
-                                   )
+                                    prix=10)
 
         r2=Ressource.objects.create(nom="mine",
-                               prix=15)
-        s1=Stock.objects.create(
-            ressource=r1,
-            quantite=50
-        )
-        s2=Stock.objects.create(
-            ressource=r2,
-            quantite=1000
-        )
+                                    prix=15)
+        s1=Stock.objects.create(ressource=r1,
+                                quantite=50)
+        s2=Stock.objects.create(ressource=r2,
+                                quantite=1000)
 self.assertEqual (Usine.objects.first().costs(), 113750)
        
      

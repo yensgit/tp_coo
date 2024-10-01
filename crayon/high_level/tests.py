d1b3966e727395175b5dd54f9bc787a33708409f
yensgit <mariamyena2@gmail.com>
2024-10-01 09:55:49 +0200

    Update tests.py

--- crayon/high_level/tests.py
+++ crayon/high_level/tests.py
@@ -1,1 +1,14 @@
 # Create your tests here.
+
+from django.test import TestCase
+
+from .models import Machine
+
+
+class MachineModelTests(TestCase):
+    def test_machine_creation(self):
+        self.assertEqual(Machine.objects.count(), 0)
+        Machine.objects.create(nom="scie",
+                               prix=1_000,
+                               n_serie=44365)
+        self.assertEqual(Machine.objects.count(), 1)

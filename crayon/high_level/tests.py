5984bea9e201d056d94fae78c21368fed1e107d1
yensgit <mariamyena2@gmail.com>
2024-10-01 10:52:43 +0200

    Update tests.py

--- crayon/high_level/tests.py
+++ crayon/high_level/tests.py
@@ -2,7 +2,7 @@
 
 from django.test import TestCase
 
-from .models import Machine
+from .models import Machine Usine
 
 
 class MachineModelTests(TestCase):
@@ -12,3 +12,22 @@ class MachineModelTests(TestCase):
                                prix=1_000,
                                n_serie=44365)
         self.assertEqual(Machine.objects.count(), 1)
+    def test_usine_costs(self): 
+        u=Usine.objects.create()
+        m1=Machine.objects.create(nom="m1",
+                               prix=1000,
+                               n_serie=1)
+        u.machines.add(m1)
+        m2=Machine.objects.create(nom="m2",
+                               prix=2000,
+                               n_serie=2)
+        u.machines.add(m2) 
+        r1=Ressource.objects.create(nom="bois",
+                               prix=10,
+                                   )
+        u.ressource.add(r1)
+         r1=Ressource.objects.create(nom="mine",
+                               prix=15,
+                                    )
+        u.ressource.add(r2)
+      

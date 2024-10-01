8d40cf2e748f0e300cbfc69bffe76d35bade4c34
yensgit <mariamyena2@gmail.com>
2024-10-01 09:24:14 +0200

    Update models.py

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -23,7 +23,7 @@ class Objet(models.Model):
     nom=models.CharField(max_length=100)
     prix=models.IntegerField(default=0)
 
-class SiegeSocial(Local):
+class Siegesocial(Local):
     pass
 
 class Ressource(models.Model):

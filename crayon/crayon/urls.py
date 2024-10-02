144ab3474ce06d9de788ca4a197f2591b98c1d2d
yensgit <mariamyena2@gmail.com>
2024-10-02 15:44:52 +0200

    Update urls.py

--- crayon/crayon/urls.py
+++ crayon/crayon/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from high_level.views import Ville
+from high_level.views import VilleJsonDetailView
 
 urlpatterns = [
     path("admin/", admin.site.urls),

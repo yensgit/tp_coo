586b55d6b5bb3da9284f42d7d257e8e5cab5d7bd
yensgit <mariamyena2@gmail.com>
2024-10-04 14:43:07 +0200

    Update low_level.cpp

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -8,4 +8,9 @@ int code_postal;
 int prix_m2;
 public:
 Ville (string n, int cp, int p):nom{n}, code_postal{cp}, prix_m2{p} {}
+friend std::ostream& operator<<(
+  std::ostream& out, cons Ville& v) {
+  return out<<v.nom<<"/"<<v.code_postal<<"/"<<v.prix_m2;
 }
+
+int main()

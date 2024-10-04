54195a348fbb1ff6f72d191dbe04ed3d08567ab3
yensgit <mariamyena2@gmail.com>
2024-10-04 14:30:23 +0200

    Update low_level.cpp

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Ville{
+string nom;
+int code_postal;
+int prix_m2;
+public:
+Ville (string n, int cp, int p):nom{n}, code_postal{cp}, prix_m2{p} {}
+}

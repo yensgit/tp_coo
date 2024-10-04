899a53ff6c8b2596327d5ec5da4a1fd21624444c
yensgit <mariamyena2@gmail.com>
2024-10-04 16:36:41 +0200

    Update low_level.cpp

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -16,7 +16,8 @@ friend std::ostream& operator<<(
   return out<<v.nom<<"/"<<v.code_postal<<"/"<<v.prix_m2;
 }
 
-Ville1(const json& data){
+Ville
+(const json& data){
   nom=data.value("nom","inconnu");
   code_postal=data.value("code postal",0);
   prix_m2=data.value("prix m2",0);}
@@ -35,7 +36,7 @@ auto main(int argc, char** argv)-> int{
   
   const auto v= Ville{j["nom"], j["code postal"], j["prix m2"]};
   std::cout<<"ville :"<< v<< std::endl;
- Ville1 v1{d};
-  std::cout<<"ville1 :"<< v1<< std::endl;
+ Ville v{d};
+  std::cout<<"ville1 :"<< v<< std::endl;
   return 0;
 }

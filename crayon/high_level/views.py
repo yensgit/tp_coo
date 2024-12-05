60096f8ad058a1fa64eceaad987a8980e25efbb4
yensgit <mariamyena2@gmail.com>
2024-12-05 10:10:25 +0100

    Update views.py

--- crayon/high_level/views.py
+++ crayon/high_level/views.py
@@ -1,10 +1,14 @@
 from .models import Ville, Local ,Objet,QuantiteRessource,Machine,Usine,Etape, Produit, Stock
 from django.http import JsonResponse
 from django.views.generic import DetailView
+#from rest_framework.views import APIView
+#from rest_framework.permissions import IsAuthenticated, AllowAny
+#from rest_framework.response import Response
+#from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
 
 class VilleJsonDetailView(DetailView): 
   model=Ville

0f76df974a305206e445f338c30cc223a140d01d
yensgit <mariamyena2@gmail.com>
2025-01-24 09:18:47 +0100

    Update low_level.cpp

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -55,7 +55,7 @@ auto main(int argc, char** argv)-> int{
   std::cout << "ville 2 : " << v2 << std::endl; 
 
   //Pour le construteur int id
-   const auto l = Local{1};
+   const auto l = Local{j};
   std::cout << "Local : " << l << std::endl; 
   return 0;
 }

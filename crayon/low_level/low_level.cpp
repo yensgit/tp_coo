d997d575aabeefdf451ac7fd4bd6d8c1edac14a3
yensgit <mariamyena2@gmail.com>
2025-01-24 09:41:10 +0100

    Update low_level.cpp

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -33,6 +33,10 @@ friend std::ostream& operator<<(
   return out<<*l.ville<<"/"<<l.nom<<"/"<<l.surface;
 }
 Local(json d) : ville{std::make_unique<Ville>(d["ville"])}, nom{d["nom"]}, surface{d["surface"]} {}
+Local(int id) {
+cpr::Response r = cpr::Get(cpr::Url{"http://127.0.0.1:8000/local/" + to_string(id) + "/"}); 
+json j = json::parse(r.text); 
+ ville= j["ville"];nom = j["nom"];surface= j["code postal"]; } 
 
 };
 auto main(int argc, char** argv)-> int{

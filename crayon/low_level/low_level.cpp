96633653dae6b7a57bf71992bc7e750be70f6ecd
yensgit <mariamyena2@gmail.com>
2025-01-27 17:10:48 +0100

    Update low_level.cpp

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -22,43 +22,6 @@ json j = json::parse(r.text);
 nom = j["nom"]; code_postal = j["code postal"]; prix_m2 = j["prix m2"]; } 
 };
 
-class Local {
- private:
-  string nom_local;
-  unique_ptr<Ville> p_ville;
-  int surface_local;
-  int ville;
-
- public:
-  Local(string nom_local_, int surface_local_, int ville_)
-      : nom_local(nom_local_), ville(ville_), surface_local(surface_local_) {
-    p_ville = make_unique<Ville>(ville);
-  }
-
-  Local(const json& data)
-      : nom_local(data["nom_local"]),
-        surface_local(data["surface_local"]),
-        ville(data["ville"]) {
-    p_ville = make_unique<Ville>(ville);
-  }
-
-  Local(int id) {
-    cpr::Response r = cpr::Get(
-        cpr::Url{"http://127.0.0.1:8000/local/" + to_string(id) + "/"});
-
-    json j = json::parse(r.text);
-    nom_local = j["nom_local"];
-    surface_local = j["surface_local"];
-    ville = j["ville"];
-    p_ville = make_unique<Ville>(ville);
-    
-  }
-  void afficher1() const {
-    cout << "Nom du local: " << nom_local << endl;
-    cout << "Surface du local: " << surface_local << " m2" << endl;
-    cout << "ID de la ville: " << ville << endl;}
-
-};
 auto main(int argc, char** argv)-> int{
    cpr::Response r = cpr::Get(cpr::Url{"http://127.0.0.1:8000/villes/1/"});
     r.status_code;                  // 200
@@ -79,8 +42,6 @@ auto main(int argc, char** argv)-> int{
   //Pour le construteur int id
    const auto v2 = Ville{2};
   std::cout << "ville 2 : " << v2 << std::endl; 
-//pour l'affichage du local
-  const auto l= Local{1};
-    l.afficher1();
+
   return 0;
 }

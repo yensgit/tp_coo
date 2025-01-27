4cea976e60f57e0675c8309853836700d6094e22
yensgit <mariamyena2@gmail.com>
2025-01-27 19:28:31 +0100

    Update low_level.cpp

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -86,7 +86,7 @@ Usine(int l) : local{std::make_unique<Local>(l)} {}
 Usine(json d)
         : local(std::make_unique<Local>(d["local"]["ville"], d["local"]["nom"], d["local"]["surface"])),
           {}
-Local(int id) {
+Usine(int id) {
         cpr::Response r = cpr::Get(cpr::Url{"http://127.0.0.1:8000/usine/" + to_string(id) + "/"});
         json j = json::parse(r.text);
         local = make_unique<Local>(j["local"]);  

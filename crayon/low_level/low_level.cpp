03e3d8bbf778d076fe34dd97ebce912cc01705ad
yensgit <mariamyena2@gmail.com>
2025-01-27 18:19:30 +0100

    Update low_level.cpp

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -31,7 +31,7 @@ friend std::ostream& operator<<(
   std::ostream& out, const Local& l) {
   return out<<*l.ville<<"/"<<l.nom<<"/"<<l.surface;
 }
-Local (json d):nom{d["nom"]},ville{d["ville"]},surface{d["surface"]} {}
+Local (json d):nom{d["nom"]}, make_unique<Ville>({d["ville"])},surface{d["surface"]} {}
 Local(int id) {
 cpr::Response r = cpr::Get(cpr::Url{"http://127.0.0.1:8000/locaux/" + to_string(id) + "/"}); 
 json j = json::parse(r.text); 

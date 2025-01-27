9f1fe6fe59bc464d61ff4fe7ac86a146dca532d6
yensgit <mariamyena2@gmail.com>
2025-01-27 18:08:56 +0100

    Update low_level.cpp

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -82,7 +82,7 @@ auto main(int argc, char** argv)-> int{
   const auto ob= Objet{j1["nom"], j1["prix"]};
   std::cout<<"objet :"<< ob<< std::endl;
   
-  const auto ob1 = Objet{1};
+  const auto ob1 = Objet{2};
   std::cout << "objet: " << ob1 << std::endl; 
   
 

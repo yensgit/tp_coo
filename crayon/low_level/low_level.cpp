aaa2ad56e59051dc8cd5461c2e608ecacd83e9e2
yensgit <mariamyena2@gmail.com>
2025-01-30 16:59:21 +0100

    Update low_level.cpp

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -17,7 +17,7 @@ public:
 Ville (string n, int cp, int p):nom{n}, code_postal{cp}, prix_m2{p} {}
 friend std::ostream& operator<<(
   std::ostream& out, const Ville& v) {
-  return out<<v.nom<<"/"<<v.code_postal<<"/"<<v.prix_m2<<"euros par m²";
+  return out<<v.nom<<"/"<<v.code_postal<<"/"<<v.prix_m2<<" euros par m²";
 }
 //Constructeur avec json data
 Ville (json d):nom{d["nom"]},code_postal{d["code postal"]},prix_m2{d["prix m2"]} {}
@@ -302,22 +302,21 @@ auto main(int argc, char** argv)-> int{
   std::cout<< r.text<< std::endl;
   json j = json::parse(r.text);
   
-    //////////////////////////OBJET//////////////////////////////////////////////
- cpr::Response r1 = cpr::Get(cpr::Url{"http://127.0.0.1:8000/objet/1/"});
+  //////////////////////////LOCAUX//////////////////////////////////////////////
+  cpr::Response r1= cpr::Get(cpr::Url{"http://127.0.0.1:8000/locaux/1/"});
   r1.status_code;                  // 200
     r1.header["content-type"];       // application/json; charset=utf-8
     r1.text;  
    std::cout<< r1.text<< std::endl;
   json j1 = json::parse(r1.text);
   
-  //////////////////////////LOCAUX//////////////////////////////////////////////
-  cpr::Response r2= cpr::Get(cpr::Url{"http://127.0.0.1:8000/locaux/1/"});
+    //////////////////////////OBJET//////////////////////////////////////////////
+ cpr::Response r2 = cpr::Get(cpr::Url{"http://127.0.0.1:8000/objet/1/"});
   r2.status_code;                  // 200
     r2.header["content-type"];       // application/json; charset=utf-8
     r2.text;  
    std::cout<< r2.text<< std::endl;
   json j2 = json::parse(r2.text);
-
     /////////////////////////MACHINE///////////////////////////////////////////// 
 
   cpr::Response r3= cpr::Get(cpr::Url{"http://127.0.0.1:8000/machine/1/"});
@@ -406,17 +405,18 @@ std::cout<<"ville 1 :"<< v1<< std::endl;
   //Pour le construteur int id
 const auto v2 = Ville{2};
 std::cout << "ville 2 : " << v2 << std::endl; 
+/////////////////////////AFFICHAGE LOCAUX///////////////////////////////
+  //Affichage avec attributs
+const auto l= Local{j1["ville"], j1["nom"], j1["surface"]};
+std::cout<<"local :"<< l<< std::endl;
  /////////////////////////AFFICHAGE OBJET///////////////////////////////  
   //Affichage avec attributs
-const auto ob= Objet{j1["nom"], j1["prix"]};
+const auto ob= Objet{j2["nom"], j2["prix"]};
 std::cout<<"objet :"<< ob<< std::endl;
   //Affichage avec int id
 const auto ob1 = Objet{2};
 std::cout << "objet: " << ob1 << std::endl; 
-/////////////////////////AFFICHAGE LOCAUX///////////////////////////////
-  //Affichage avec attributs
-const auto l= Local{j2["ville"], j2["nom"], j2["surface"]};
-std::cout<<"local :"<< l<< std::endl;
+
   
 /////////////////////////AFFICHAGE MACHINE///////////////////////////////
   //Affichage avec attributs
